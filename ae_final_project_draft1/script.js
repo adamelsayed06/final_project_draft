ea539c4678def4335f0cf30feee482781568ed24
Adam Elsayed <107322895+adamelsayed06@users.noreply.github.com>
2024-01-08 10:58:51 -0500

    Add files via upload

---
+++ ae_final_project_draft1/script.js
@@ -0,0 +1,6 @@
+const button1 = document.getElementById('button1');
+
+button1.addEventListener('click', function onClick() {
+  button1.style.backgroundColor = 'salmon';
+  button1.style.color = 'white';
+});
